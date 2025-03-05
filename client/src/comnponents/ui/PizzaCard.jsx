82d22cdf085bcf9cd54ab9cd40747d947234f48e
alex-zander-0403 <alexzander0403@gmail.com>
2025-03-05 20:27:26 +0300

    adaptive css fix

--- client/src/comnponents/ui/PizzaCard.jsx
+++ client/src/comnponents/ui/PizzaCard.jsx
@@ -15,57 +15,59 @@ export default function PizzaCard({ el }) {
 
   //
   return (
-    <div className="pizza-card">
-      <img className="pizza-card__image" src={el.imageUrl} alt="Pizza" />
-      <h4 className="pizza-card__title">{el.title}</h4>
-      <div className="pizza-card__selector">
-        {/*  */}
-        <ul>
-          {el.types.map((type, i) => (
-            <li
-              className={activeType === i ? "active" : ""}
-              key={i}
-              onClick={() => setActiveType(i)}
-            >
-              {typeNames[type]}
-            </li>
-          ))}
-        </ul>
-        {/*  */}
-        <ul>
-          {el.sizes.map((size, i) => (
-            <li
-              className={activeSize === i ? "active" : ""}
-              key={i}
-              onClick={() => setActiveSize(i)}
-            >
-              {size} см
-            </li>
-          ))}
-        </ul>
-        {/*  */}
-      </div>
-      <div className="pizza-card__bottom">
-        <div className="pizza-card__price">от {el.price} ₽</div>
-        <button
-          onClick={onClickAdd}
-          className="button button--outline button--add"
-        >
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+    <div className="pizza-card-wrapper">
+      <div className="pizza-card">
+        <img className="pizza-card__image" src={el.imageUrl} alt="Pizza" />
+        <h4 className="pizza-card__title">{el.title}</h4>
+        <div className="pizza-card__selector">
+          {/*  */}
+          <ul>
+            {el.types.map((type, i) => (
+              <li
+                className={activeType === i ? "active" : ""}
+                key={i}
+                onClick={() => setActiveType(i)}
+              >
+                {typeNames[type]}
+              </li>
+            ))}
+          </ul>
+          {/*  */}
+          <ul>
+            {el.sizes.map((size, i) => (
+              <li
+                className={activeSize === i ? "active" : ""}
+                key={i}
+                onClick={() => setActiveSize(i)}
+              >
+                {size} см
+              </li>
+            ))}
+          </ul>
+          {/*  */}
+        </div>
+        <div className="pizza-card__bottom">
+          <div className="pizza-card__price">от {el.price} ₽</div>
+          <button
+            onClick={onClickAdd}
+            className="button button--outline button--add"
           >
-            <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
-            />
-          </svg>
-          <span>Добавить</span>
-          <i>{pizzaCount}</i>
-        </button>
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                fill="white"
+              />
+            </svg>
+            <span>Добавить</span>
+            <i>{pizzaCount}</i>
+          </button>
+        </div>
       </div>
     </div>
   );
